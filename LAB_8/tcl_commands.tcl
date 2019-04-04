df75d4275c9c758b99710595887410795a89495c
Rajbir Malik <cs1170416@iitd.ac.in>
2019-04-04 15:56:57 +0530

    Added TCL commands for smooth simulation

---
+++ LAB_8/tcl_commands.tcl
@@ -0,0 +1,37 @@
+# TCL commands for simulation on vivado
+
+# Setting clock (10ns - 5ns)
+add_force {/TestBench/test_clock} -radix hex {0 0ns} {1 5000ps} -repeat_every 10000ps
+
+# Setting go command
+add_force {/TestBench/go} -radix bin {0 0ns}
+add_force {/TestBench/go} -radix bin {1 0ns}
+
+# Setting step command
+add_force {/TestBench/step} -radix bin {0 0ns}
+add_force {/TestBench/step} -radix bin {1 0ns}
+
+# Setting instr command
+add_force {/TestBench/instr} -radix bin {0 0ns}
+add_force {/TestBench/instr} -radix bin {1 0ns}
+
+# Setting the reset command
+add_force {/TestBench/reset} -radix bin {0 0ns}
+add_force {/TestBench/reset} -radix bin {1 0ns}
+
+# Complete Reset
+add_force {/TestBench/step} -radix bin {0 0ns}
+run 10ns
+add_force {/TestBench/go} -radix bin {0 0ns}
+run 10ns
+add_force {/TestBench/instr} -radix bin {0 0ns}
+run 10ns
+add_force {/TestBench/reset} -radix bin {1 0ns}
+run 10ns
+
+# Program Select
+# Enter the program no. in place
+add_force {/TestBench/Program_Select} -radix bin {111 0ns}
+add_force {/TestBench/reset} -radix bin {1 10ns}
+add_force {/TestBench/reset} -radix bin {0 50ns}
+run 60ns
